09d240e9ba82cdbb54dfb4cf8e1756b65a42b569
dhkdchqh <57143470+dhkdchqh@users.noreply.github.com>
2019-11-16 03:48:56 +0900

    더블 버퍼링

--- Project1/game.c
+++ Project1/game.c
@@ -10,17 +10,27 @@
 #define WALL '#'
 #define PORTAL '@'
 
+static HANDLE g_hScreen[2];
+int screenIndex = 0;
+
 char map[N][3 * N];
 
-int canJump;
+int canJump = 0;
+int jumping = 0;
 
 typedef struct {
 	int x, y;
 	char ch;
+	int collision;
+	int alive;
 } object;
 
 int player_startPos_X = 0;
-int player_startPos_Y = 0;
+int player_startPos_Y = N - 2;
+int time;
+char key;
+object player;
+object monster;
 
 void gotoxy(int x, int);
 void cursor(int n);
@@ -29,15 +39,69 @@ void moveN(int x, int y, int n, object* obj);
 void drawMap(void);
 int isCollideWith(char block, int x, int y, object* obj);
 int isCollideWithMap(int x, int y, object* obj);
+int isCollideWithObj(object* obj1, int x, int y, object* obj2);
+void kill(object* obj);
 
-int main(void)
+void init(void);
+void playerControl(void);
+void monsterControl(void);
+void drawTime(void);
+void WriteBuffer(int n, int x, int y, char str[]);
+
+
+int main()
 {
-	char key;
-	object player;
+	int time2, cnt = 0;
+	g_hScreen[0] = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	g_hScreen[1] = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	init();
+
+	while (1)
+	{
+		//drawTime();
+		playerControl();
+		if (time % 10 == 0) {
+			//system("cls");
+			drawMap();
+		}
+		if (time % 20 == 0) {
+			if (!jumping) {
+				move(0, 1, &player);
+			}
+		}
+		if (jumping) {
+			if (!cnt) {
+				time2 = time;
+			}
+			if ((time - time2) % 20 == 0) {
+				move(0, -1, &player);
+				cnt++;
+			}
+			if (cnt == 3) {
+				jumping = 0;
+				cnt = 0;
+			}
+		}
+		if (monster.alive && time % 200 == 0) {
+			monsterControl(&monster);
+		}
+		Sleep(10);
+		time += 10;
+	}
+	return 0;
+}
+
+void init(void)
+{
 	player.ch = 'P';
 	player.x = player_startPos_X;
 	player.y = player_startPos_Y;
-
+	player.alive = 1;
+	monster.ch = 'M';
+	monster.x = 6;
+	monster.y = N - 2;
+	monster.alive = 1;
+	time = 0;
 	for (int j = 0; j < 3 * N; j++) {
 		map[N - 1][j] = WALL;
 	}
@@ -46,37 +110,47 @@ int main(void)
 	map[N - 4][7] = WALL;
 	map[N - 4][8] = WALL;
 	map[N - 4][9] = WALL;
-	map[N - 2][18] = PORTAL;
+	map[N - 6][10] = WALL;
+	map[N - 3][18] = PORTAL;
 
 	cursor(0);
 	drawMap();
 	move(0, 0, &player);
-	while (1) {
-		if (kbhit()) {
-			key = getch();
-			switch (key) {
-			case 'w':
-				if (canJump) {
-					moveN(0, -1, 3, &player);
-				}
-				break;
-			case 'a':
-				move(-1, 0, &player);
-				break;
-			case 'd':
-				move(1, 0, &player);
-				break;
-			default:
-				break;
+	move(0, 0, &monster);
+}
+
+void playerControl(void)
+{
+	if (kbhit()) {
+		key = getch();
+		switch (key) {
+		case 'w':
+			if (canJump) {
+				jumping = 1;
+				canJump = 0;
 			}
-		}
-		else {
-			Sleep(50);
-			move(0, 1, &player);
+			break;
+		case 'a':
+			move(-1, 0, &player);
+			break;
+		case 'd':
+			move(1, 0, &player);
+			break;
+		default:
+			break;
 		}
 	}
+}
 
-	return 0;
+void monsterControl(object* monster)
+{
+	static int x = 1;
+	if (isCollideWith(WALL, x, 0, monster) || isCollideWithMap(x, 0, monster)) x = -x;
+	if (isCollideWith(player.ch, 0, -1, monster)) {
+		kill(monster);
+		return;
+	}
+	move(x, 0, monster);
 }
 
 void gotoxy(int x, int y) // 커서 위치를 이동시킨다.
@@ -97,17 +171,27 @@ void cursor(int n) // 0 : 커서 사라짐 / 1 : 커서 생김
 
 void move(int x, int y, object* obj)
 {
-	if (isCollideWithMap(x, y, obj)) return;
+	if (isCollideWithMap(x, y, obj)) {
+		return;
+		obj->collision = 1;
+	}
 	if (isCollideWith(WALL, x, y, obj)) {
 		if (y > 0) canJump = 1;
+		obj->collision = 1;
 		return;
 	}
 	if (isCollideWith(WALL, x, y + 1, obj)) {
 		canJump = 1;
 	}
-	gotoxy(obj->x, obj->y);
-	printf("%c", BACKGROUND);
-	if (isCollideWith(PORTAL, x, y, obj)) {
+	if (obj->ch == player.ch && isCollideWith(monster.ch, x, y + 1, obj)) {
+		move(0, -1, obj);
+		//kill(&monster);
+		return;
+	}
+	map[obj->y][obj->x] = BACKGROUND;
+	if (isCollideWith(PORTAL, x, y, obj) || isCollideWith(monster.ch, x, y, obj)) {
+		//init();
+		//return;
 		obj->x = player_startPos_X;
 		obj->y = player_startPos_Y;
 		x = 0;
@@ -115,32 +199,65 @@ void move(int x, int y, object* obj)
 	}
 	obj->x += x;
 	obj->y += y;
-	gotoxy(obj->x, obj->y);
-	printf("%c", obj->ch);
+	map[obj->y][obj->x] = obj->ch;
+
 }
 
 void moveN(int x, int y, int n, object* obj)
 {
 	for (int i = 0; i < n; i++) {
-		Sleep(50);
+		//Sleep(50);
 		move(0, -1, obj);
 		canJump = 0;
 	}
 }
 
+//void drawMap(void)
+//{
+//	int start = 0;
+//	if (player.x > N / 2) start = player.x - N / 2;
+//	if (player.x > 2 * N + N / 2) start = 2 * N;
+//	if (start + N)
+//		for (int i = 0; i < N; i++) {
+//			for (int j = start; j < start + N; j++) {
+//				printf("%c%c", map[i][j], map[i][j]);
+//			}
+//			printf("\n");
+//		}
+//}
+
 void drawMap(void)
 {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < 3 * N; j++) {
-			printf("%c", map[i][j]);
+	int start = 0;
+	screenIndex = !screenIndex;
+	if (player.x > N / 2) start = player.x - N / 2;
+	if (player.x > 2 * N + N / 2) start = 2 * N;
+	if (start + N)
+		for (int i = 0; i < N; i++) {
+			for (int j = start, k = 0; j < start + N; j++, k += 2) {
+				WriteBuffer(screenIndex, k, i, &map[i][j]);
+				WriteBuffer(screenIndex, k + 1, i, &map[i][j]);
+			}
 		}
-		printf("\n");
-	}
+	SetConsoleActiveScreenBuffer(g_hScreen[screenIndex]);
 }
 
-int isCollideWith(char block, int x, int y, object* obj)
+
+void drawTime(void)
+{
+	gotoxy(0, 0);
+	printf("TIME : %d", time / 100);
+}
+
+int isCollideWith(char block, int x, int y, object * obj)
 {
 	if ((map[obj->y + y][obj->x + x] == block)) return 1;
+	else return 0;
+}
+
+int isCollideWithObj(object * obj1, int x, int y, object * obj2)
+{
+	if ((map[obj1->y + y][obj1->x + x] == obj2->ch)) return 1;
 	else return 0;
 }
 
@@ -148,4 +265,18 @@ int isCollideWithMap(int x, int y, object * obj)
 {
 	if (obj->x + x < 0 || obj->x + x > 3 * N - 1 || obj->y + y < 0 || obj->y + y > N - 1) return 1;
 	else return 0;
+}
+
+void kill(object * obj)
+{
+	obj->alive = 0;
+	map[obj->y][obj->x] = BACKGROUND;
+}
+
+void WriteBuffer(int n, int x, int y, char* ch) // 버퍼 내용 입력 함수
+{
+	DWORD dw;
+	COORD CursorPosition = { x, y };
+	SetConsoleCursorPosition(g_hScreen[n], CursorPosition);
+	WriteFile(g_hScreen[n], ch, 1, &dw, NULL);
 }
